d3834fad0ca8ba6d062afb54bfc2e73935875c07
Maarten in 't Hout <maarten@twiks.nl>
2013-07-10 10:58:42 +0200

    Send done if steps is 0

--- motorcontrol.py
+++ motorcontrol.py
@@ -1,7 +1,6 @@
 __author__ = 'Maarten'
 
 import time
-import math
 
 import serial
 from multibus import BusCore, BusServer, BusClient
@@ -37,14 +36,13 @@ def findController():
             pass
 
 
-
 def sendAction(ser, motor, angle):
     #Send actions
     ser.write(chr(0xF0))
     ser.write(chr(motor))
-    if(angle < 0):
+    if (angle < 0):
         ser.write(chr(0x02))
-    if(angle > 0):
+    if (angle > 0):
         ser.write(chr(0x01))
     ser.write(str(abs(angle)) + "\n")
     ser.write(chr(0x0F))
@@ -63,7 +61,7 @@ if __name__ == '__main__':
     print("Motor controller Server: init")
     s = BusServer.BusServer(15001)
     s.listen()
-    myAngle = {0x01: 0,0x02: 0,0x03: 0}
+    myAngle = {0x01: 0, 0x02: 0, 0x03: 0}
     while True:
         packet = s.getPacket()
         if packet.action == BusCore.PacketType.SETMOTOR:
@@ -76,18 +74,25 @@ if __name__ == '__main__':
             angle = packet.data['angle']
             steps = angle - myAngle[motor]
             myAngle[motor] = angle;
-            steps = round(steps * (1600.0/360.0))
+            steps = round(steps * (1600.0 / 360.0))
             if packet.data['motor'] == 'A':
-                steps = round(steps * (70.0/15.0))
+                steps = round(steps * (70.0 / 15.0))
 
             print str(steps) + "\n"
-            ser = findController()
-            if ser is not None:
-                result = sendAction(ser, motor, steps)
-                if packet.data['returnport'] is not None:
-                    c = BusClient.BusClient(packet.data['returnport'])
-                    try:
-                        c.send(BusCore.Packet(BusCore.PacketType.SETMOTOR, {'result': result}))
-                    except:
-                        print("Motor controller Server: failed to send done")
-                ser.close()
+            if steps != 0:
+                ser = findController()
+                if ser is not None:
+                    result = sendAction(ser, motor, steps)
+                    if packet.data['returnport'] is not None:
+                        c = BusClient.BusClient(packet.data['returnport'])
+                        try:
+                            c.send(BusCore.Packet(BusCore.PacketType.SETMOTOR, {'result': result}))
+                        except:
+                            print("Motor controller Server: failed to send done")
+                    ser.close()
+            else:
+                try:
+                    c.send(BusCore.Packet(BusCore.PacketType.SETMOTOR, {'result': True}))
+                except:
+                    print("Motor controller Server: failed to send done")
+
